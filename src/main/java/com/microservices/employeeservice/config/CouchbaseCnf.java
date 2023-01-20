294470b4dbbb03457e25af7a62f17cd925114c61
weeb-coder <weebcoder1@gmail.com>
2023-01-20 14:15:52 +0530

    Modified

--- src/main/java/com/microservices/employeeservice/config/CouchbaseCnf.java
+++ src/main/java/com/microservices/employeeservice/config/CouchbaseCnf.java
@@ -1,7 +1,6 @@
 package com.microservices.employeeservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -13,19 +12,6 @@ public class CouchbaseCnf extends AbstractCouchbaseConfiguration {
 
     @Autowired
     private Environment env;
-
-//    @Value("{app.couchbase.host}")
-//    private String connectionString;
-//
-//    @Value("{app.couchbase.username}")
-//    private String username;
-//
-//    @Value("{app.couchbase.password}")
-//    private String password;
-//
-//    @Value("{app.couchbase.bucket-employee}")
-//    private String employeeBucket;
-
 
     @Override
     public String getConnectionString() {

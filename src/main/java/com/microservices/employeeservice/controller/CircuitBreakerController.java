e2b67936e27eaa55c3fd14ab1f1ec11571853584
weeb-coder <weebcoder1@gmail.com>
2023-01-23 16:50:44 +0530

    Modified

---
+++ src/main/java/com/microservices/employeeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,23 @@
+package com.microservices.employeeservice.controller;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    @GetMapping("/sample-api")
+//    @Retry(name = "sample-api", fallbackMethod = "fallbackResponse")
+    @CircuitBreaker(name = "sample-api", fallbackMethod = "fallbackResponse")
+    public String sampleApi() {
+        return new RestTemplate().getForEntity("http://localhost:8080/some-endpoint", String.class).getBody();
+    }
+
+    public String fallbackResponse(Exception e) {
+        return "This is a fallback method yet to be implemented";
+    }
+
+}
